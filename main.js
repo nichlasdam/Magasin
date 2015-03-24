782e03de4d0afc5519ae2b33879ca1eedb88a93c
nichlasdam <nichlas@damgruppen.dk>
2015-03-24 14:12:44 +0100

    Added some styling to frontpage

---
+++ main.js
@@ -0,0 +1,8 @@
+function subscribe() {
+    localStorage.subscribe = "Ja";
+    console.log("Nu kører det.")
+}
+function ignore() {
+    localStorage.subscribe = "Nej";
+    console.log("Ignore!");
+}
